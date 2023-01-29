e04e53ab52eb19ad7f0a214a7f0f5ea240619db6
Bea Tendero <beatriz.tendero.hernandez@gmail.com>
2023-01-29 13:04:34 +0100

    error corregido

--- migrations/04-create-prestamo.js
+++ migrations/04-create-prestamo.js
@@ -2,14 +2,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('prestamos', {
+    await queryInterface.createTable('prestamo', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      titulo: {
+      title: {
         type: Sequelize.INTEGER
       },
       fecha: {
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('prestamos');
+    await queryInterface.dropTable('prestamo');
   }
 };
